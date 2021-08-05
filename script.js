969b5f6ff2f95684e24b3d990f07208e27edebe8
Khai Nguyen <nguyentrongkhaiptit@gmail.com>
2021-08-05 22:08:47 +0700

    smooth scrolling, scrollspy, overflow-x, underline

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     $(window).scroll(function() {
         let position = $(this).scrollTop();
         // console.log(position);
-        if (position >= 650 && position <= 1850) {
+        if (position >= 650 && position <= 1830) {
             $('.camera-img').addClass('flyInFromLeft');
             $('.mission-text').addClass('flyInFromRight');
         } else {
@@ -38,7 +38,7 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
         let position = $(this).scrollTop();
-        if (position >= 4350 && position <= 6065) {
+        if (position >= 4330 && position <= 6065) {
             $('.card-1').addClass('cardMoveFromLeft');
             $('.card-2').addClass('cardMoveFromBottom');
             $('.card-3').addClass('cardMoveFromRight');
@@ -48,4 +48,36 @@ $(document).ready(function() {
             $('.card-3').removeClass('cardMoveFromRight');
         }
     });
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    // $(".scroll-area").scrollspy({ target: "#myNavbar" })
+    // $('body').scrollspy({ target: '#navbarScroll', offset: 10 });
+    // $('[data-spy="scroll"]').each(function() {
+    //     var $spy = $(this).scrollspy('refresh')
+    // });
+    // $('[data-spy="scroll"]').on('activate.bs.scrollspy', function() {
+    //     // do something…
+    //     console.log("log");
+    // })
 });
